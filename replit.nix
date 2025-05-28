c76255977acf5812995009ae0b34d9519d664570
daveynj113 <27202854-daveynj113@users.noreply.replit.com>
2025-05-28 04:11:51 +0000

    bundle reduction removed pdf stuff

--- replit.nix
+++ replit.nix
@@ -1,17 +1,12 @@
 { pkgs }: {
   deps = [
-    pkgs.firefox
     pkgs.nodejs-18_x
-    pkgs.chromium
     pkgs.postgresql
     pkgs.which
     pkgs.findutils
   ];
   
   env = {
-    CHROME_BIN = "${pkgs.chromium}/bin/chromium";
-    CHROMIUM_BIN = "${pkgs.chromium}/bin/chromium";
-    PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = "true";
-    PUPPETEER_EXECUTABLE_PATH = "${pkgs.chromium}/bin/chromium";
+    # Removed browser automation dependencies for lighter deployment
   };
 } 
